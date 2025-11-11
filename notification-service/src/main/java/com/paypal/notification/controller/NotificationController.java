ab1724e0c27a3a252fac1dbf7412044c01031a58
FrostScythe <maheshwariayush371@gmail.com>
2025-11-11 17:46:15 +0530

    notification-service

---
+++ notification-service/src/main/java/com/paypal/notification/controller/NotificationController.java
@@ -0,0 +1,26 @@
+package com.paypal.notification.controller;
+
+import com.paypal.notification.entity.Notification;
+import com.paypal.notification.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/notify")
+public class NotificationController {
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @PostMapping
+    public Notification sendNotification(@RequestBody Notification notification) {
+        return notificationService.sendNotification(notification);
+    }
+
+    @GetMapping("/{userId}")
+    public List<Notification> getNotificationsByUser(@PathVariable Long userId) {
+        return notificationService.getNotificationsByUserId(userId);
+    }
+}
---
+++ notification-service/src/main/java/com/paypal/notification/service/NotificationService.java
@@ -0,0 +1,11 @@
+package com.paypal.notification.service;
+
+import com.paypal.notification.entity.Notification;
+
+import java.util.List;
+
+public interface NotificationService {
+    Notification sendNotification(Notification notification);
+    List<Notification> getNotificationsByUserId(Long userId);
+
+}
